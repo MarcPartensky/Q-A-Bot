bb9528e362b3e154cf9d5542fb6540070d32013d
marc <marc.partensky@gmail.com>
2020-12-14 13:54:11 +0100

    new requirements

--- server.py
+++ server.py
@@ -1,5 +1,17 @@
-from Flask import app, render_template
+from flask import Flask, render_template
+from flask_restful import Resource, Api
 
+app = Flask(__name__)
+api = Api(app)
+
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello': 'world'}
+
+api.add_resource(HelloWorld, '/')
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
 
 
